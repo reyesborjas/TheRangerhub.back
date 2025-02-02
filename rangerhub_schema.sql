8d96cbb141f61902aa7aadaa4a961915615f24be
José Reyes <reyesborjas@gmail.com>
2025-02-02 19:19:44 -0300

    Update rangerhub_schema.sql

--- rangerhub_schema.sql
+++ rangerhub_schema.sql
@@ -1,117 +1,126 @@
--- Habilitar la extensión UUID
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Tabla de roles de usuario (debe ir ANTES de users ya que users la referencia)
+
 CREATE TABLE user_roles (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    role_name VARCHAR(10) NOT NULL UNIQUE,
-    description varchar(50) NOT NULL
+    role_name VARCHAR(25) NOT NULL UNIQUE,
+    description VARCHAR(50) NOT NULL
 );
 
--- Tabla de usuarios
+
 CREATE TABLE users (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    username VARCHAR(10) NOT NULL UNIQUE,
-    first_name varchar(20) NOT NULL,
-    last_name varchar(20) NOT NULL,
-    nationality varchar(20) NOT NULL,
-    rut varchar(11),
-    passport_number varchar(20),
-    role_id uuid NOT NULL REFERENCES user_roles(id),
-    profession varchar(50),
+    username VARCHAR(100) NOT NULL UNIQUE,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    nationality varchar(50) NOT NULL,
+    rut varchar(25) UNIQUE,
+    passport_number varchar(25) UNIQUE,
+     role_id uuid NOT NULL REFERENCES user_roles(id) on DELETE CASCADE,
     biography text,
-    email varchar(100) NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
     availability_start_date date,
-    availability_end_date date
+    availability_end_date date,
+    user_status VARCHAR(50) NOT NULL DEFAULT 'activo',
+    profile_picture_url varchar(255) UNIQUE,
+    profile_visibility BOOLEAN NOT NULL DEFAULT TRUE   
 );
 
--- Tabla de viajes
+
 CREATE TABLE trips (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     start_date timestamptz NOT NULL,
     end_date timestamptz NOT NULL,
     participants_number integer NOT NULL,
-    status varchar(25) DEFAULT 'pending' NOT NULL, 
-    estimated_weather_forecast text,
-    description text NOT NULL,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz,
-    total_cost numeric(10,2)
+    trip_status VARCHAR(50) NOT NULL DEFAULT 'pendiente',
+    estimated_weather_forecast jsonb,
+    description text ,
+    created_at TIMESTAMP with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP with time zone,
+    total_cost numeric(10,2),
+    trip_image_url varchar(255)
 );
 
--- Tabla de pagos
+
 CREATE TABLE payment (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    user_id uuid NOT NULL,
-    trip_id uuid NOT NULL,
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
+    trip_id uuid NOT NULL REFERENCES trips(id) ON DELETE NO ACTION,
     payment_amount numeric(10,2),
     payment_method VARCHAR(50),
     payment_date date,
-    payment_bill_url VARCHAR(255),
-    payment_voucher_url varchar(255),
+    payment_voucher_url varchar(255) UNIQUE,
     payment_status VARCHAR(50)
 );
 
--- Tabla de reservas
+
 CREATE TABLE reservations (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    trip_id uuid NOT NULL REFERENCES trips(id),
-    user_id uuid NOT NULL REFERENCES users(id),
-    status varchar(255) DEFAULT 'reserved' NOT NULL
+    trip_id uuid NOT NULL REFERENCES trips(id) ON DELETE CASCADE,
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    status VARCHAR(50) NOT NULL DEFAULT 'pendiente'
 );
 
--- Tabla de ubicaciones
+
 CREATE TABLE locations (
    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-   place_name varchar(30) NOT NULL UNIQUE,
-   place_type VARCHAR(15) NOT NULL,
-   country varchar(50) NOT NULL,
-   province varchar(50) NOT NULL,
-   nearest_city varchar(50) NOT NULL,
-   coordinates point NOT NULL
+   place_name varchar(255) NOT NULL UNIQUE,
+   place_type VARCHAR(50) NOT NULL,
+   country varchar(100) NOT NULL,
+   province varchar(100) NOT NULL,
+   nearest_city varchar(100) NOT NULL,
+   coordinates point NOT NULL,
+   location_image_url varchar(255)
 );
 
--- Tabla de actividades
+CREATE TABLE activity_categories (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description TEXT
+);
+
 CREATE TABLE activities (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    location_id uuid NOT NULL REFERENCES locations(id),
-    name VARCHAR(20) NOT NULL UNIQUE,
-    description varchar(50) NOT NULL,
+    category_id UUID REFERENCES activity_categories(id) ON DELETE SET NULL,
+    location_id uuid NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description varchar(255) NOT NULL,
     duration numeric(5,2) NOT NULL,
-    difficulty char(1) NOT NULL,
+    difficulty varchar(20) NOT NULL,
     min_participants integer NOT NULL,
     max_participants integer NOT NULL,
     cancellation_policy text NOT NULL,
     is_available boolean NOT NULL DEFAULT TRUE,
     is_public boolean NOT NULL DEFAULT TRUE,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz NOT NULL,
-    cost numeric(10,2) NOT NULL
+    created_at TIMESTAMP with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP with time zone,
+    cost numeric(10,2) NOT NULL,
+    activity_image_url varchar(255) UNIQUE
 );
 
--- Tabla puente para actividades y viajes
+
 CREATE TABLE activity_trips (
    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-   activity_id uuid NOT NULL REFERENCES activities(id),
-   trip_id uuid NOT NULL REFERENCES trips(id)
+   activity_id uuid NOT NULL REFERENCES activities(id) ON DELETE CASCADE,
+   trip_id uuid NOT NULL REFERENCES trips(id) ON DELETE CASCADE
 );
 
--- Tabla de recursos
 CREATE TABLE resources (
    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
    name varchar(100) NOT NULL UNIQUE,
    description jsonb NOT NULL,
-   cost numeric(10,2) NOT NULL
+   cost numeric(10,2) NOT NULL,
 );
 
--- Tabla puente para recursos y viajes
+
 CREATE TABLE trip_resources (
    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
    resource_id uuid NOT NULL REFERENCES resources(id),
    trip_id uuid NOT NULL REFERENCES trips(id)
 );
 
--- Tabla de certificaciones
+
 CREATE TABLE certifications (
    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
    issued_by varchar(100) NOT NULL,
@@ -119,32 +128,15 @@ CREATE TABLE certifications (
    valid_until date NOT NULL,
    certification_number varchar(50),
    document_url varchar(255),
-   created_at date NOT NULL,
-   title VARCHAR(100) UNIQUE
+   created_at date NOT NULL DEFAULT CURRENT_DATE,
+   title VARCHAR(100),
+   UNIQUE (title, certification_number, valid_until)
 );
 
--- Tabla puente para certificaciones y rangers (usuarios)
 CREATE TABLE ranger_certifications (
-   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-   certification_id uuid NOT NULL REFERENCES certifications(id),
-   user_id uuid NOT NULL REFERENCES users(id)
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    certification_id uuid NOT NULL REFERENCES certifications(id),
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE(certification_id, user_id)
 );
 
--- Constraints adicionales
-ALTER TABLE users
-    ADD CONSTRAINT fk_user_role
-    FOREIGN KEY (role_id)
-    REFERENCES user_roles(id)
-    ON DELETE CASCADE;
-
-ALTER TABLE payment
-    ADD CONSTRAINT fk_user_payment
-    FOREIGN KEY (user_id)
-    REFERENCES users(id)
-    ON DELETE RESTRICT;
-
-ALTER TABLE payment
-    ADD CONSTRAINT fk_trip_payment
-    FOREIGN KEY (trip_id)
-    REFERENCES trips(id)
-    ON DELETE NO ACTION;
