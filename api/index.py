876312094968757c68d5d3477dc479b83fda8971
reyesborjas <reyesborjas@gmail.com>
2025-02-21 20:36:54 -0300

    get activities get trips

--- api/index.py
+++ api/index.py
@@ -67,8 +67,7 @@ def register():
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (body["username"], body["first_name"], body["last_name"], body["nationality"], 
               body["rut"], body["passport_number"], body["role_id"], 
-              body["biography"], body["email"], hashed_password 
-              ))
+              body["biography"], body["email"], hashed_password))
         
         connection.commit()
         return jsonify({"message": "Usuario creado correctamente"}), 201
@@ -90,9 +89,7 @@ def get_roles():
     try:
         cursor.execute("SELECT * FROM user_roles")
         roles = cursor.fetchall()
-        if not roles:
-            return jsonify({"message": "No hay roles disponibles"}), 404
-        return jsonify({"roles": roles}), 200
+        return jsonify({"roles": roles if roles else []}), 200
     except Exception as e:
         logging.error(f"Error al obtener roles: {e}")
         return jsonify({"message": "Error al obtener los roles"}), 500
@@ -115,10 +112,9 @@ def login():
         if not username or not password:
             return jsonify({"message": "Debe ingresar usuario y contraseña"}), 400
        
-        hashed_password = hash_password(password)  # Hashear la contraseña ingresada
+        hashed_password = hash_password(password)
        
-        cursor.execute("SELECT id, username, role_id, password FROM users WHERE username = %s", 
-                       (username,))
+        cursor.execute("SELECT id, username, role_id, password FROM users WHERE username = %s", (username,))
         user = cursor.fetchone()
         
         if user and user["password"] == hashed_password:
@@ -130,8 +126,7 @@ def login():
             }, SECRET_KEY, algorithm="HS256")
 
             return jsonify({"message": "Login exitoso", "token": token}), 200
-        else:
-            return jsonify({"message": "Credenciales incorrectas"}), 401
+        return jsonify({"message": "Credenciales incorrectas"}), 401
 
     except Exception as e:
         logging.error(f"Error en el login: {e}")
@@ -140,172 +135,41 @@ def login():
         cursor.close()
         connection.close()
 
-@app.route('/activities', methods=['POST', 'GET'])
-def create_activity():
-    connection = get_db_connection()
-    if not connection:
-        return jsonify({"message": "Error de conexión con la base de datos"}), 500
-
-    cursor = connection.cursor()
-    try:
-        if request.method == 'POST':
-            body = request.get_json()
-            print("Datos recibidos:", body)  # Registra los datos recibidos
-
-            # Validar tipos de datos
-            try:
-                duration = float(body["duration"])
-                min_participants = int(body["min_participants"])
-                max_participants = int(body["max_participants"])
-                cost = float(body["cost"])
-            except (ValueError, KeyError) as e:
-                return jsonify({"message": f"Error en los tipos de datos: {str(e)}"}), 400
-
-            # Inserción de la actividad en la tabla activities
-            cursor.execute("""
-                INSERT INTO activities (
-                    category_id, location_id, name, description, duration, difficulty, 
-                    min_participants, max_participants, is_available, 
-                    is_public, cost, activity_image_url
-                ) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                RETURNING id
-            """, (
-                body["category_id"], body["location_id"], body["name"], body["description"], 
-                duration, body["difficulty"], min_participants, max_participants, 
-                body["is_available"], body["is_public"], cost, 
-                body.get("activity_image_url")  # Enviar NULL si no se proporciona
-            ))
-
-            activity_id = cursor.fetchone()[0]  # Obtenemos el id de la actividad recién insertada
-            connection.commit()
-
-            return jsonify({
-                "message": "Actividad creada correctamente",
-                "activity_id": activity_id
-            }), 201
-
-        elif request.method == 'GET':
-            cursor.execute("SELECT * FROM activities")
-            activities = cursor.fetchall()
-
-            if not activities:
-                return jsonify({"message": "No hay actividades disponibles"}), 404
-
-            return jsonify({"activities": activities}), 200
-
-    except Exception as e:
-        logging.error(f"Error al crear o consultar la actividad: {e}")
-        return jsonify({"message": f"Error en el servidor: {str(e)}"}), 500
-    finally:
-        cursor.close()
-        connection.close()
-
-@app.route('/activity_trips', methods=['POST'])
-def associate_activity_trip():
-    """Asocia una actividad a un viaje"""
-    connection = get_db_connection()
-    if not connection:
-        return jsonify({"message": "Error de conexión con la base de datos"}), 500
-
-    cursor = connection.cursor()
-    try:
-        body = request.get_json()
-        print("Datos recibidos para asociar actividad y viaje:", body)
-
-        # Inserción en la tabla activity_trips
-        cursor.execute("""
-            INSERT INTO activity_trips (activity_id, trip_id)
-            VALUES (%s, %s)
-        """, (body["activity_id"], body["trip_id"]))
-
-        connection.commit()
-        return jsonify({"message": "Actividad asociada al viaje correctamente"}), 201
-
-    except Exception as e:
-        logging.error(f"Error al asociar actividad y viaje: {e}")
-        return jsonify({"message": "Error al asociar actividad y viaje"}), 500
-    finally:
-        cursor.close()
-        connection.close()
-
-@app.route('/activitycategory', methods=['GET'])  
-def get_activity_categories():  
-    """Obtiene todas las categorías de actividades"""  
+@app.route('/trips', methods=['GET'])
+def get_trips():  
     connection = get_db_connection()  
     if not connection:  
         return jsonify({"message": "Error de conexión con la base de datos"}), 500  
 
     cursor = connection.cursor()
     try:  
-        cursor.execute("SELECT * FROM activity_categories")  
-        activity_categories = cursor.fetchall()  
+        cursor.execute("SELECT * FROM trips")  
+        trips = cursor.fetchall()  
 
-        if not activity_categories:  
-            return jsonify({"message": "No hay categorías disponibles"}), 404  
-
-        return jsonify({"categories": activity_categories}), 200  
+        return jsonify({"trips": trips if trips else []}), 200  
     except Exception as e:  
-        logging.error(f"Error al obtener categorías: {e}")  
-        return jsonify({"message": "Error al obtener las categorías"}), 500  
+        logging.error(f"Error al obtener los viajes: {e}")  
+        return jsonify({"message": "Error al obtener los viajes"}), 500  
     finally:  
         cursor.close()
         connection.close()
 
-@app.route('/locations', methods=['GET'])
-def get_locations():
-    connection = get_db_connection()
-    if not connection:
-        logging.error("❌ Error: No se pudo conectar a la base de datos")
-        return jsonify({"message": "Error de conexión con la base de datos"}), 500
-
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    
-    try:
-        cursor.execute("SELECT * FROM locations")
-        locations = cursor.fetchall()
-
-        if not locations:
-            logging.warning("⚠️ No hay localizaciones disponibles en la BD")
-            return jsonify({"message": "No hay localizaciones disponibles"}), 404
-
-        # Convertir cada fila en un diccionario y devolver un array
-        locations_list = [dict(row) for row in locations]
-        return jsonify({"locations": locations_list}), 200
-    
-    except Exception as e: 
-        logging.error(f"Error al obtener localizaciones: {e}")
-        return jsonify({"message": "Error al obtener las localizaciones"}), 500
-    finally:
-        cursor.close()
-        connection.close()
-
-@app.route('/trips', methods=['POST'])
-def create_trip():
-    """Crea un nuevo viaje"""
+@app.route('/resources', methods=['GET'])
+def get_resources():
+    """Obtiene todos los recursos"""
     connection = get_db_connection()
     if not connection:
         return jsonify({"message": "Error de conexión con la base de datos"}), 500
 
     cursor = connection.cursor()
     try:
-        body = request.get_json()
+        cursor.execute("SELECT * FROM resources")
+        resources = cursor.fetchall()
 
-        cursor.execute("""
-            INSERT INTO trips (
-                trip_name, description, start_date, end_date, trip_status, 
-                participants_number, estimated_weather_forecast, total_cost, trip_image_url
-            ) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (body["trip_name"], body["description"], body["start_date"], body["end_date"], 
-              body["trip_status"], body["participants_number"], body["estimated_weather_forecast"], 
-              body["total_cost"], body["trip_image_url"]))
-
-        connection.commit()
-        return jsonify({"message": "Viaje creado correctamente"}), 201
+        return jsonify({"resources": resources if resources else []}), 200
     except Exception as e:
-        logging.error(f"Error al crear el viaje: {e}")
-        return jsonify({"message": "Error al crear el viaje"}), 500
+        logging.error(f"Error al obtener los recursos: {e}")
+        return jsonify({"message": "Error al obtener los recursos"}), 500
     finally:
         cursor.close()
         connection.close()
@@ -318,25 +182,22 @@ def get_rangers():
 
     cursor = connection.cursor()
     try:
-        ranger_role_id = uuid.UUID('8f285ee6-7ded-473d-8c57-5159a489e7e6')
+        ranger_role_id = "8f285ee6-7ded-473d-8c57-5159a489e7e6"
 
         cursor.execute("""
             SELECT first_name, last_name, username 
             FROM users 
-            WHERE role_id = %s::uuid
-        """, (str(ranger_role_id),))
+            WHERE role_id = %s
+        """, (ranger_role_id,))
 
         rangers = cursor.fetchall()
-
-        if not rangers:
-            return jsonify({"message": "No hay rangers disponibles"}), 404
 
         formatted_rangers = [
             {"full_name": f"{r['first_name']} {r['last_name']} ({r['username']})"}
             for r in rangers
         ]
 
-        return jsonify({"rangers": formatted_rangers}), 200
+        return jsonify({"rangers": formatted_rangers if formatted_rangers else []}), 200
 
     except Exception as e:
         logging.error(f"Error al obtener rangers: {e}")
